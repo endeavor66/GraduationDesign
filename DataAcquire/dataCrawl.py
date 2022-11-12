937a66cfe874fccc513b81473fc2992bb8fb01f9
mwg <mwg@whut.edu.com>
2022-11-12 22:13:33 +0800

    调整dataCrawl中部分变量名

--- DataAcquire/dataCrawl.py
+++ DataAcquire/dataCrawl.py
@@ -1,43 +1,52 @@
 from utils.gharchive_utils import get_client, join_commits_sha
 from utils.mysql_utils import insert_batch
+from datetime import datetime
 
 repo = 'tensorflow/tensorflow'
 
 # 查询数据
+start = datetime.now()
+
 gh = get_client()
-data = gh.get('2021-12-01 08:00:00', '2022-12-01 12:00:00', filters=[
+archive = gh.get('2021-12-01 08:00:00', '2021-12-01 10:00:00', filters=[
     ('repo.name', repo)
 ])
 
+end = datetime.now()
+print("一共查询到 %d 条数据, 耗时 %d s" % (len(archive.data), (end-start).seconds))
+
 # 保存到数据库
 datas = []
-for archive in data:
-    t = (archive.id,
-         archive.type,
-         archive.public,
-         archive.created_at,
-         archive.actor.id,
-         archive.actor.login,
-         archive.repo.id,
-         archive.repo.name,
-         archive.payload.ref,
-         archive.payload.ref_type,
-         archive.payload.pusher_type,
-         archive.payload.push_id,
-         archive.payload.size,
-         archive.payload.distinct_size,
-         join_commits_sha(archive.payload.commits),
-         archive.payload.action,
-         archive.payload.number,
-         archive.payload.changes,
-         archive.member.id if archive.member is not None else None,
-         archive.member.login if archive.member is not None else None,
-         archive.member.type if archive.member is not None else None,
-         archive.member.site_admin if archive.member is not None else None
+for archive_element in archive:
+    t = (archive_element.id,
+         archive_element.type,
+         archive_element.public,
+         archive_element.created_at,
+         archive_element.actor.id,
+         archive_element.actor.login,
+         archive_element.repo.id,
+         archive_element.repo.name,
+         archive_element.payload.ref,
+         archive_element.payload.ref_type,
+         archive_element.payload.pusher_type,
+         archive_element.payload.push_id,
+         archive_element.payload.size,
+         archive_element.payload.distinct_size,
+         join_commits_sha(archive_element.payload.commits),
+         archive_element.payload.action,
+         archive_element.payload.number,
+         archive_element.payload.changes,
+         archive_element.member.id if archive_element.member is not None else None,
+         archive_element.member.login if archive_element.member is not None else None,
+         archive_element.member.type if archive_element.member is not None else None,
+         archive_element.member.site_admin if archive_element.member is not None else None
     )
     datas.append(t)
 
+start = datetime.now()
 insert_batch("events", datas, repo[repo.index('/')+1:])
+end = datetime.now()
+print("插入数据库耗时 %d s" % (end-start).seconds)
 
 
 
--- utils/access_key.py
+++ utils/access_key.py
@@ -2,15 +2,15 @@ import os
 
 # 获取本地文件中的access_tocken相关值
 def get_token(num=0):
-    filepath = "D:\\PythonProject\\GraduationDesign\\utils\\token.txt"
-    with open("token.txt", 'r') as reader:
+    filepath = "D:\\PythonProject\\GraduationDesign\\utils\\resources\\token.txt"
+    with open(filepath, 'r') as reader:
         token = reader.readline()
     return token
 
 
 # 获取本地文件中的access_tocken相关值
 def get_mysql_root_psw():
-    filepath = "D:\\PythonProject\\GraduationDesign\\utils\\mysql.txt"
+    filepath = "D:\\PythonProject\\GraduationDesign\\utils\\resources\\mysql.txt"
     with open(filepath, 'r') as reader:
         username = reader.readline().strip("\n")
         password = reader.readline().strip("\n")
