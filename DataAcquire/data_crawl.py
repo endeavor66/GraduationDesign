e7d4449592d1a7793f9cbef950c0023030175fb9
mwg <mwg@whut.edu.com>
2022-11-19 10:53:20 +0800

    更新data_crawl.py和data_process.py脚本，进一步封装API，简化调用

--- DataAcquire/data_crawl.py
+++ DataAcquire/data_crawl.py
@@ -1,54 +1,63 @@
 from utils.gharchive_utils import get_client, join_commits_sha
 from utils.mysql_utils import insert_batch
-from datetime import datetime
+from datetime import datetime, timedelta
 
-repo = 'tensorflow/tensorflow'
 
-# 查询数据
-start = datetime.now()
+'''
+功能：通过GHArchive client查询特定仓库repo在特定时间段(start_time, end_time)的数据，结果保存到数据库。以一天作为一个批次执行插入
+'''
+def crawl_data(repo: str, start_time: datetime, end_time: datetime):
+    gh = get_client()
+    start = start_time
+    end = min(end_time, start + timedelta(hours=23))
+    while start <= end_time:
+        print(f"start: {start}, end: {end}")
+        # 1.通过GHArchive client查询数据
+        archive = gh.get(start, end, filters=[('repo.name', repo)])
+        print("一共查询到 %d 条数据" % len(archive.data))
+        # 2.提取关键活动，封装为元组list
+        datas = []
+        for archive_element in archive:
+            t = (archive_element.id,
+                 archive_element.type,
+                 archive_element.public,
+                 archive_element.created_at,
+                 archive_element.actor.id,
+                 archive_element.actor.login,
+                 archive_element.repo.id,
+                 archive_element.repo.name,
+                 archive_element.payload.ref,
+                 archive_element.payload.ref_type,
+                 archive_element.payload.pusher_type,
+                 archive_element.payload.push_id,
+                 archive_element.payload.size,
+                 archive_element.payload.distinct_size,
+                 join_commits_sha(archive_element.payload.commits),
+                 archive_element.payload.action,
+                 archive_element.payload.pull_request.number if archive_element.payload.pull_request is not None else None,
+                 archive_element.payload.forkee.full_name if archive_element.payload.forkee is not None else None,
+                 archive_element.payload.changes,
+                 # PullRequestReviewEvent相关
+                 archive_element.payload.review.state if archive_element.payload.review is not None else None,
+                 archive_element.payload.review.author_association if archive_element.payload.review is not None else None,
+                 # MemberEvent相关
+                 archive_element.payload.member.id if archive_element.payload.member is not None else None,
+                 archive_element.payload.member.login if archive_element.payload.member is not None else None,
+                 archive_element.payload.member.type if archive_element.payload.member is not None else None,
+                 archive_element.payload.member.site_admin if archive_element.payload.member is not None else None
+                 )
+            datas.append(t)
+        # 3.保存到数据库
+        insert_batch("events", datas, repo[repo.index('/') + 1:])
+        del archive
+        del datas
+        # 4.更新新一轮的时间段
+        start = start + timedelta(days=1)
+        end = min(end_time, start + timedelta(hours=23))
 
-gh = get_client()
-archive = gh.get('2021-12-01 00:00:00', '2021-12-01 23:00:00', filters=[
-    ('repo.name', repo)
-])
 
-end = datetime.now()
-print("一共查询到 %d 条数据, 耗时 %d s" % (len(archive.data), (end - start).seconds))
-
-# 保存到数据库
-datas = []
-for archive_element in archive:
-    t = (archive_element.id,
-         archive_element.type,
-         archive_element.public,
-         archive_element.created_at,
-         archive_element.actor.id,
-         archive_element.actor.login,
-         archive_element.repo.id,
-         archive_element.repo.name,
-         archive_element.payload.ref,
-         archive_element.payload.ref_type,
-         archive_element.payload.pusher_type,
-         archive_element.payload.push_id,
-         archive_element.payload.size,
-         archive_element.payload.distinct_size,
-         join_commits_sha(archive_element.payload.commits),
-         archive_element.payload.action,
-         archive_element.payload.pull_request.number if archive_element.payload.pull_request is not None else None,
-         archive_element.payload.forkee.full_name if archive_element.payload.forkee is not None else None,
-         archive_element.payload.changes,
-         # PullRequestReviewEvent相关
-         archive_element.payload.review.state if archive_element.payload.review is not None else None,
-         archive_element.payload.review.author_association if archive_element.payload.review is not None else None,
-         # MemberEvent相关
-         archive_element.payload.member.id if archive_element.payload.member is not None else None,
-         archive_element.payload.member.login if archive_element.payload.member is not None else None,
-         archive_element.payload.member.type if archive_element.payload.member is not None else None,
-         archive_element.payload.member.site_admin if archive_element.payload.member is not None else None
-         )
-    datas.append(t)
-
-start = datetime.now()
-insert_batch("events", datas, repo[repo.index('/') + 1:])
-end = datetime.now()
-print("插入数据库耗时 %d s" % (end - start).seconds)
+if __name__ == '__main__':
+    repo = 'tensorflow/tensorflow'
+    start_time = datetime(2021, 12, 11, 1, 0, 0)  # 年，月，日，时，分，秒 其中年，月，日是必须的
+    end_time = datetime(2021, 12, 13, 5, 0, 0)
+    crawl_data(repo, start_time, end_time)
--- DataAcquire/data_extract_from_bigquery.py
+++ DataAcquire/data_extract_from_bigquery.py
@@ -2,10 +2,6 @@ import pandas as pd
 import json
 from utils.mysql_utils import insert_batch
 
-repo = "tensorflow/tensorflow"
-filepath = "bigquery_data/20211223.csv"
-df = pd.read_csv(filepath)
-df = df.loc[df['repo_name'] == repo]
 
 '''
 功能：从commits中提取所有commit的SHA，拼接为字符串后返回(#连接)，格式：SHA1#SHA2#SHA3
@@ -29,32 +25,48 @@ def check(v):
     return v
 
 
-datas = []
-for index, event in df.iterrows():
-    t = (check(event['id']),
-         check(event['type']),
-         check(event['public']),
-         check(event['created_at']),
-         check(event['actor_id']),
-         check(event['actor_login']),
-         check(event['repo_id']),
-         check(event['repo_name']),
-         check(event['payload_ref']),
-         check(event['payload_ref_type']),
-         check(event['payload_pusher_type']),
-         check(event['payload_push_id']),
-         check(event['payload_size']),
-         check(event['payload_distinct_size']),
-         join_commits_sha(check(event['payload_commits'])),
-         check(event['payload_action']),
-         check(event['payload_pr_number']),
-         check(event['payload_forkee_full_name']),
-         check(event['payload_changes']),
-         check(event['payload_member_id']),
-         check(event['payload_member_login']),
-         check(event['payload_member_type']),
-         check(event['payload_member_site_admin'])
-         )
-    datas.append(t)
+'''
+功能：从csv文件(文件路径为filepath)中提取repo所有相关的事件，保存到数据库
+'''
+def extract_data_from_bigquery_csv(repo: str, filepath: str):
+    # 1.从csv文件中筛选出repo所有相关事件
+    df = pd.read_csv(filepath)
+    df = df.loc[df['repo_name'] == repo]
+    # 2.提取关键活动
+    datas = []
+    for index, event in df.iterrows():
+        t = (check(event['id']),
+             check(event['type']),
+             check(event['public']),
+             check(event['created_at']),
+             check(event['actor_id']),
+             check(event['actor_login']),
+             check(event['repo_id']),
+             check(event['repo_name']),
+             check(event['payload_ref']),
+             check(event['payload_ref_type']),
+             check(event['payload_pusher_type']),
+             check(event['payload_push_id']),
+             check(event['payload_size']),
+             check(event['payload_distinct_size']),
+             join_commits_sha(check(event['payload_commits'])),
+             check(event['payload_action']),
+             check(event['payload_pr_number']),
+             check(event['payload_forkee_full_name']),
+             check(event['payload_changes']),
+             check(event['payload_review_state']),
+             check(event['payload_review_author_association']),
+             check(event['payload_member_id']),
+             check(event['payload_member_login']),
+             check(event['payload_member_type']),
+             check(event['payload_member_site_admin'])
+             )
+        datas.append(t)
+    # 3.保存到数据库
+    insert_batch("events", datas, repo[repo.index('/') + 1:])
 
-insert_batch("events", datas, repo[repo.index('/') + 1:])
+
+if __name__ == '__main__':
+    repo = "tensorflow/tensorflow"
+    filepath = "bigquery_data/20211210.csv"
+    extract_data_from_bigquery_csv(repo, filepath)
